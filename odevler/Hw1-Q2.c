9d8e3e2368f7e0f89d94dbebf03613733ba6104c
Süleyman Altun <altunmanx@gmail.com>
2025-03-22 15:59:16 +0300

    Update Hw1-Q2.c

--- odevler/Hw1-Q2.c
+++ odevler/Hw1-Q2.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <math.h>
+int main( void )
+{
+ int popularity;     //popularity
+ int size;           //size
+ int homeValue;      // summation popular�ity and size ant multiplication 10000
+ 
+ printf("Enter the popularity: \n " );
+ scanf( "%d", &popularity );
+ 
+ printf("Enter the size: \n ");
+ scanf("%d", &size );
+ 
+ homeValue = (int)(pow(popularity, 3) + pow(size, 2))*10000;
+
+ printf("Home value is: %d \n", homeValue); 
+ 
+ return 0;
+ 
+}
